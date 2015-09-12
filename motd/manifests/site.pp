267288e8790183bedb3085db7f377674a4243a07
AbbeySun <abbeysun@sina.com>
2015-09-12 21:39:13 +0800

    change site.pp file

--- motd/manifests/site.pp
+++ motd/manifests/site.pp
@@ -1,8 +1,13 @@
-$puppetserver = 'puppetmaster.abbeysun.com' # set global variable
+$puppetserver = 'puppetmaster.abbeysun.com' #设置全局变量
 node 'puppetmaster_cert.abbeysun.com'{
   include  motd
 }
 node 'agent1_cert.abbeysun.com'{
   include  motd
-
+}
+node 'agent2_cert.abbeysun.com'{
+  include  motd
+}
+node 'agent3_cert.abbeysun.com'{
+  include  motd
 }
