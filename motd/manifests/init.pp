c332cceab12f0770cfd6bcee989e9d75d53439b0
AbbeySun <abbeysun@sina.com>
2015-09-12 21:57:21 +0800

    change log

--- motd/manifests/init.pp
+++ motd/manifests/init.pp
@@ -1,6 +1,6 @@
 class motd{            # define a 'motd' class
   package{ 'setup':    # define package resource
-    ensure =>absent , # require a 'setup' package need to be installed.
+    ensure =>present , # require a 'setup' package need to be installed.
   }
   file{ '/etc/motd':     # define file resouce
     ensure  => present,  # require '/etc/motd'file need to be present.
